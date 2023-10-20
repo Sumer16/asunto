20fb65fda10b8ba5ae99ee95f5fd153ba3bbff88
Sumer Patel <patelsum@oregonstate.edu>
2023-10-20 00:18:46 -0700

    Added Issue APIs for posting issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import * as z from 'zod';
+import prisma from '@/prisma/client';
+
+const createIssueSchema = z.object({
+  title: z.string().min(1, {
+    message: 'Issue name is required.'
+  }).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+  
+    const validation = createIssueSchema.safeParse(body);
+  
+    if (!validation.success) {
+      return NextResponse.json(validation.error.errors, { status: 400 });
+    }
+  
+    const newIssue = await prisma.issue.create({
+      data: {
+        title: body.title,
+        description: body.description,
+      }
+    });
+  
+    return NextResponse.json(newIssue, { status: 201 });
+  } catch (error) {
+    console.log(error);    
+  }
+}
