e2615bf6aa57d5ca015fd2d0fec09a9b0bccb966
Sumer Patel <patelsum@oregonstate.edu>
2023-10-21 02:24:21 -0700

    Added markdown previewer for issues card

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation';
 import prisma from '@/prisma/client';
+import ReactMarkdown from 'react-markdown';
 
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import { Card, Flex, Heading, Text } from '@radix-ui/themes';
@@ -19,12 +20,12 @@ const IssueDetailPage = async ({ params }: Props) => {
   return (
     <div>
       <Heading>{issue.title}</Heading>
-      <Flex className="space-x-3" my="2">
+      <Flex className='space-x-3' my='2'>
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>
-        <p>{issue.description}</p>
+      <Card className='prose' mt='4'>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
   )
