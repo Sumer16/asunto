b3ffe15ea8cdaf54c1003565b08df4fe72a81477
Sumer Patel <patelsum@oregonstate.edu>
2023-10-22 18:12:30 -0700

    Removed duplicate skeletons

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -14,7 +14,7 @@ const NavBar = () => {
 
   const links = [
     { label: 'Dashboard', href: '/' },
-    { label: 'Issues', href: '/issues' },
+    { label: 'Issues', href: '/issues/list' },
   ];
   
   return (
