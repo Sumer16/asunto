1abb54a8e33a62471be575c9af563666c7fefd25
Sumer Patel <patelsum@oregonstate.edu>
2023-10-25 03:32:00 -0700

    Added Issue Summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import prisma from '@/prisma/client';
+
+import IssueSummary from './IssueSummary';
 import LatestIssues from './LatestIssues';
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: 'OPEN' },
+  });
+
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  
+  const closed = await prisma.issue.count({
+    where: { status: 'CLOSED' },
+  });
+
   return (
-    <LatestIssues />
+    <>
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+      <LatestIssues />
+    </>
   );
 }
