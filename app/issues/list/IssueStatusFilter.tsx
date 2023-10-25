73afe7c928f4190137c23c4424a9e3bafa79f078
Sumer Patel <patelsum@oregonstate.edu>
2023-10-25 02:14:23 -0700

    Fix:Filtering Bug

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,25 +1,34 @@
 'use client';
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 import { Status } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
 
-const statuses: { label: string, value?: Status }[] = [
-   { label: 'All' },
-   { label: 'Open', value: 'OPEN' },
-   { label: 'In Progress', value: 'IN_PROGRESS' },
-   { label: 'Closed', value: 'CLOSED' },
+const statuses: { label: string; value?: Status }[] = [
+  { label: 'All' },
+  { label: 'Open', value: 'OPEN' },
+  { label: 'In Progress', value: 'IN_PROGRESS' },
+  { label: 'Closed', value: 'CLOSED' },
 ];
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
+      defaultValue={searchParams.get('status') || ''}
       onValueChange={(status) => {
-        const query = status ? `?status=${status}` : '';
+        const params = new URLSearchParams();
+
+        if (status) params.append('status', status);
+
+        if (searchParams.get('orderBy'))
+          params.append('orderBy', searchParams.get('orderBy')!);
+
+        const query = params.size ? '?' + params.toString() : '';
         router.push('/issues/list' + query);
       }}
     >
